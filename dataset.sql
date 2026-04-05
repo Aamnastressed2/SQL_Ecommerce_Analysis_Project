0b30458df8a4e0c85062c5476ca9d537f62af1b6
aamna-sql-portfolio <121760928+Aamnastressed2@users.noreply.github.com>
2026-04-05 16:48:18 +0500

    dataset.sql

--- dataset.sql
+++ dataset.sql
@@ -1,31 +1,59 @@
-CREATE TABLE employees1 (
-    emp_id INT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    department VARCHAR(50),
-    salary INT,
-    age INT
+CREATE TABLE customers (
+    customer_id INT,
+    name VARCHAR(50),
+    country VARCHAR(50)
 );
 
-INSERT INTO employees VALUES
-(1, 'Ayesha', 'Khan', 'HR', 6000, 28),
-(2, 'Ali', 'Raza', 'IT', 4500, 32),
-(3, 'Sara', 'Ahmed', 'Finance', 7000, 40),
-(4, 'Usman', 'Ali', 'IT', 3000, 25),
-(5, 'Hina', 'Sheikh', 'HR', 5200, 35),
-(6, 'Bilal', 'Malik', 'Finance', 800, 29),
-(7, 'Zara', 'Iqbal', 'IT', 9000, 45),
-(8, 'Omar', 'Farooq', 'HR', 1500, 38),
-(9, 'Noor', 'Hassan', 'Finance', 11000, 50),
-(10, 'Danish', 'Aslam', 'IT', 2000, 27);
+INSERT INTO customers VALUES
+(1, 'Ayesha', 'Pakistan'),
+(2, 'Ali', 'UAE'),
+(3, 'Sara', 'Pakistan'),
+(4, 'John', 'USA'),
+(5, 'Emma', 'UK');
 
-CREATE TABLE departments1 (
-    department VARCHAR(50),
-    manager_name VARCHAR(50)
+CREATE TABLE orders (
+    order_id INT,
+    customer_id INT,
+    order_date DATE,
+    amount INT
 );
 
-INSERT INTO departments VALUES
-('HR', 'Fatima'),
-('IT', 'Usman'),
-('Finance', 'Ahmed'),
-('Marketing', 'Zoya');
+INSERT INTO orders VALUES
+(101, 1, '2023-01-10', 5000),
+(102, 2, '2023-01-15', 7000),
+(103, 1, '2023-02-10', 3000),
+(104, 3, '2023-02-12', 8000),
+(105, 4, '2023-03-01', 2000),
+(106, 5, '2023-03-05', 9000),
+(107, 2, '2023-03-10', 4000),
+(108, 3, '2023-04-01', 6000);
+
+CREATE TABLE products (
+    product_id INT,
+    product_name VARCHAR(50),
+    category VARCHAR(50),
+    price INT
+);
+
+INSERT INTO products VALUES
+(1, 'Laptop', 'Electronics', 1000),
+(2, 'Phone', 'Electronics', 800),
+(3, 'Shoes', 'Fashion', 200),
+(4, 'Watch', 'Accessories', 300);
+
+
+CREATE TABLE order_items (
+    order_id INT,
+    product_id INT,
+    quantity INT
+);
+
+INSERT INTO order_items VALUES
+(101, 1, 1),
+(102, 2, 2),
+(103, 3, 3),
+(104, 1, 1),
+(105, 4, 2),
+(106, 2, 1),
+(107, 3, 2),
+(108, 1, 1);
